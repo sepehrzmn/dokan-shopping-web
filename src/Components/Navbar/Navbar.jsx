df3acb265b2a2ad62788d41af0f4ce66af39b16b
sepehr <szamani3045@gmail.com>
2022-05-04 21:56:34 +0430

    updated Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
+
 function Navbar() {
-	return <div>Navbar</div>;
+	return (
+		<nav className="nav">
+			<div className="nav-content">
+				<div className="logo">
+					<img src={logo} alt="دکان" />
+					<span>دکان</span>
+				</div>
+				<ul className="menu">
+					<li className="active">
+						<Link to="/">خانه</Link>
+					</li>
+					<li>
+						<Link to="/category">دسته بندی</Link>
+					</li>
+					<li>
+						<Link to="/mag">وبلاگ</Link>
+					</li>
+				</ul>
+				<div className="user">user</div>
+			</div>
+		</nav>
+	);
 }
 
 export default Navbar;
