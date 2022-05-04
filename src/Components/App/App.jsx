073301d0178e5308def7446f01a07c03c252dd40
sepehr <szamani3045@gmail.com>
2022-05-04 22:00:01 +0430

    added Home page

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import "./App.css";
 import "../../assets/font/Yekan.css";
 import Navbar from "../Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "../Home/Home";
 
 export default function App() {
 	return (
 		<>
 			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
 		</>
 	);
 }
