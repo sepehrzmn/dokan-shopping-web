6a99876fee177b3d015d9f2de6f29ad2e02475aa
sepehr <szamani3045@gmail.com>
2022-05-04 20:29:44 +0430

    added Navbar

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./App.css";
 import "../../assets/font/Yekan.css";
+import Navbar from "../Navbar/Navbar";
 
 export default function App() {
-	return <></>;
+	return (
+		<>
+			<Navbar />
+		</>
+	);
 }
