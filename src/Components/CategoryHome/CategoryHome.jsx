078beee6636ecea3f0fe352cabe8329d5d0dcd3e
sepehr <szamani3045@gmail.com>
2022-05-05 18:19:56 +0430

    updated CategoryHome

--- src/Components/CategoryHome/CategoryHome.jsx
+++ src/Components/CategoryHome/CategoryHome.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import "./CategoryHome.css";
+import { Icon } from "@iconify/react";
+import { Pagination, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/bundle";
 
 function CategoryHome() {
-	return <div>CategoryHome</div>;
+	return (
+		<>
+			<div className="categoryHome">
+				<div className="popular-category">
+					{Array(5)
+						.fill("")
+						.map((x, index) => {
+							return (
+								<div key={index} className="p-c-item">
+									<Icon
+										icon="emojione-monotone:violin"
+										color="white"
+										fontSize={80}
+									/>
+
+									<span>ویالون</span>
+								</div>
+							);
+						})}
+				</div>
+			</div>
+			<Swiper
+				modules={[Pagination, A11y]}
+				style={{ padding: "0 10px" }}
+				spaceBetween={50}
+				slidesPerView={3}
+				pagination={{ clickable: true }}
+			>
+				{Array(16)
+					.fill("")
+					.map((x, index) => {
+						return <></>;
+					})}
+				<SwiperSlide>Slide 1</SwiperSlide>
+			</Swiper>
+		</>
+	);
 }
 
 export default CategoryHome;
--- src/Components/Invitation/Invitation.jsx
+++ src/Components/Invitation/Invitation.jsx
@@ -3,17 +3,17 @@ import "./Invitation.css";
 import arrowDown from "../../assets/image/arrowDown.png";
 function Invitation() {
 	return (
-		<div className="invitation">
-			<h2>با صدای دلنشین و زیبای موسیقی را با ما تجربه کنید</h2>
-			<p>
-				با انواع الات موسیقی را بهترین کیفیت و ضمانت یک ساله از ما تهیه کنید
-			</p>
-			<img src={arrowDown} alt="arrow down" />
-			<div>
-				<button className="btn-product">محصولات</button>
-				<button className="btn-invitation">همکاری با ما</button>
+		<>
+			<div className="invitation">
+				<h2>با صدای دلنشین و زیبای موسیقی را با ما تجربه کنید</h2>
+				<p>انواع الات موسیقی را بهترین کیفیت و ضمانت یک ساله از ما تهیه کنید</p>
+				<div>
+					<img src={arrowDown} alt="arrow down" />
+					<button className="btn-product">محصولات</button>
+					<button className="btn-invitation">همکاری با ما</button>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
