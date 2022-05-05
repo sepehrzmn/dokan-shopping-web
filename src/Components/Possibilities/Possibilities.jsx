0c21da293c92fffcecba13dae8c6967293212d7c
sepehr <szamani3045@gmail.com>
2022-05-05 22:49:52 +0430

    updated possibilities

--- src/Components/Possibilities/Possibilities.jsx
+++ src/Components/Possibilities/Possibilities.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "./Possibilities.css";
+import concert from "../../assets/image/concert.png";
+import tutorial from "../../assets/image/tutorial.png";
 
 function Possibilities() {
-	return <div>Possibilities</div>;
+	return (
+		<div className="possibilities">
+			<div className="p-tutorial">
+				<div style={{ background: `url(${tutorial})` }} />
+				<button className="PT-btn">
+					<p>آموزش موسیقی</p>
+				</button>
+			</div>
+			<div className="p-concert">
+				<div style={{ background: `url(${concert})` }} />
+				<button className="PC-btn">
+					<p>کنسرت</p>
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default Possibilities;
